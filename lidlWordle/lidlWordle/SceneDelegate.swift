14d5e1458b136a698709495e6e2d5cf84758edd0
ketamine <vladg3@mail.ru>
2024-09-21 19:41:29 +0700

    initial commit

---
+++ lidlWordle/lidlWordle/SceneDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  SceneDelegate.swift
+//  lidlWordle
+//
+//  Created by Vladislav Mishukov on 21.09.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+
+        // Save changes in the application's managed object context when the application transitions to the background.
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+
+
+}
+
